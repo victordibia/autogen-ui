d43a58014c8e3d44aac6f3e2649d5ffd5e3ba9c5
Victor Dibia <victordibia@microsoft.com>
2023-10-24 16:59:04 -0700

    update ui build

---
+++ autogenui/web/ui/_next/static/chunks/pages/_app-451d704a741dc8a8.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{41597:function(_,n,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return e(28955)}])}},function(_){var __webpack_exec__=function(n){return _(_.s=n)};_.O(0,[774,179],function(){return __webpack_exec__(41597),__webpack_exec__(24751)}),_N_E=_.O()}]);
