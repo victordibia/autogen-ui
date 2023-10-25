308a379342a49d7651fc3c8ede849c0b4ae4257e
Victor Dibia <victordibia@microsoft.com>
2023-10-24 17:00:50 -0700

    frontend refactor

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true }
+  output: "export",
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,15 +1,40 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+import type { Config } from "tailwindcss";
 
-    // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx,mdx}'
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {}
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      transitionProperty: {
+        height: "height",
+        spacing: "margin, padding",
+      },
+      backgroundColor: {
+        primary: "var(--color-bg-primary)",
+        secondary: "var(--color-bg-secondary)",
+        accent: "var(--color-bg-accent)",
+        light: "var(--color-bg-light)",
+      },
+      textColor: {
+        accent: "var(--color-text-accent)",
+        primary: "var(--color-text-primary)",
+        secondary: "var(--color-text-secondary)",
+      },
+      borderColor: {
+        accent: "var(--color-border-accent)",
+        primary: "var(--color-border-primary)",
+        secondary: "var(--color-border-secondary)",
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
+export default config;
